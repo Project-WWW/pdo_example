89e1cc0ad7aabf70915cf2c61ff058376ec4ea50
Project-WWW <91307684+Project-WWW@users.noreply.github.com>
2022-03-28 09:32:37 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=company';
+$username = 'root';
+$password = '';
+
+$connection = new PDO($dsn, $username, $password);
+?>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" 
+          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body class="bg-warning">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link bg-success mx-2" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link bg-info" href="create.php">Add a person</a>
+      </li>
+    </ul>
+</nav>
